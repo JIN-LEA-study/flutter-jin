191e40c24fc7cfc76139f2470f3f04fecadf104f
greencoffee1635 <166354@naver.com>
2022-04-19 17:58:52 +0900

    위젯 넣기 (Text, Icon, Image, Container, Center)

--- contact/lib/main.dart
+++ contact/lib/main.dart
@@ -9,8 +9,16 @@ class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
+
     return MaterialApp(
-      home: Text('안녕')
+      // Flutter에서 앱 디자인 넣는 법 : 위젯 짜깁기
+      home: Center(
+        child: Container(width : 50, height : 50, color: Colors.blue),
+      ) // 사이즈 단위는 LP 50LP==1.2cm(얇은 검지손가락)
+      // home: Text('안녕')
+      // home: Icon(Icons.shop)
+      // home: Image.asset('assets/dog.jpg')
+
     );
   }
 }
