c48e952479f6b455281102bf0d71b7387c55cc3e
greencoffee1635 <166354@naver.com>
2022-04-20 21:07:34 +0900

    간단한 상품목록 레이아웃

--- contact/lib/main.dart
+++ contact/lib/main.dart
@@ -14,16 +14,33 @@ class MyApp extends StatelessWidget {
       // Flutter에서 앱 디자인 넣는 법 : 위젯 짜깁기
       home: Scaffold( // App을 상, 중, 하로 나눠준다.
         appBar: AppBar(),
-        body: Row(
-          children : [
-            Expanded( child: Container(color : Colors.blue), flex : 1 ),
-            Container( color : Colors.green, width : 100 ),
-            // 박스 폭을 50%로 설정하려면 Flexible()로 감싼다.
-            // Flexible( child: Container(color : Colors.blue), flex : 3 ),
-            // Flexible( child: Container(color : Colors.green), flex : 7 ),
-
-            // console => alt + 4
-            ],
+        body: Container(
+          height: 150,
+          padding: EdgeInsets.all(20),
+          child: Row(
+            children: [
+              Image.asset('dog.jpg', width: 150),
+              Container(
+                width: 300,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start, // x축 정렬
+                  children: [
+                    Text('코인팝니다', style: TextStyle()),
+                    Text('중구'),
+                    Text('10,000원'),
+                    Row(
+                    // 하트 Icon 우측 이동 => Row안에서 mainAxis정렬
+                      mainAxisAlignment: MainAxisAlignment.end, // Row안에서 가로축(x) 정렬
+                      children: [
+                        Icon(Icons.favorite),
+                        Text('4'),
+                      ],
+                    )
+                  ],
+                )
+              )
+            ]
+          )
         )
       )
     );
