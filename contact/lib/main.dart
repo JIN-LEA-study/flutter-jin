5d173eb1bc0c9ec0daf08c581a5360402fe8e143
greencoffee1635 <166354@naver.com>
2022-04-20 18:58:12 +0900

    margin, padding, border 추가

--- contact/lib/main.dart
+++ contact/lib/main.dart
@@ -14,28 +14,16 @@ class MyApp extends StatelessWidget {
       // Flutter에서 앱 디자인 넣는 법 : 위젯 짜깁기
       home: Scaffold( // App을 상, 중, 하로 나눠준다.
         appBar: AppBar(title: Text('앱임')),
-        body: Text('안녕'),
-        bottomNavigationBar: BottomAppBar(
-        // child: Container( // Container는 무거운 박스라서 Lint가 잡아준다. (밑줄생김)
-        child: SizedBox( // width, height, child만 필요한 박스는 SizedBox()
-            height: 100,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              // Row 안에서 정렬한다. (mainAxisAlignment)
-              children: [
-                Icon(Icons.phone),
-                Icon(Icons.message),
-                Icon(Icons.contact_page),
-              ],
-            ),
-          ),
-        ),
-      ) // 사이즈 단위는 LP 50LP==1.2cm(얇은 검지손가락)
-      // home: Center(
-      // child: Container(width : 50, height : 50, color: Colors.blue),
-      // home: Text('안녕')
-      // home: Icon(Icons.shop)
-      // home: Image.asset('assets/dog.jpg')
+        body: Container(
+          width: 150, height: 50,
+          margin: EdgeInsets.all(20), // 바깥 여백 주고 싶으면, margin Parameter를 이용한다.
+          // padding: EdgeInsets.fromLTRB(left, top, right, bottom),
+          // padding: EdgeInsets.fromLTRB(0, 30, 0, 0),
+          decoration: BoxDecoration( // 테두리 주고 싶으면 decoration Parameter를 준다.
+            border: Border.all(color: Colors.black)
+          )
+        )
+      )
     );
   }
 }
