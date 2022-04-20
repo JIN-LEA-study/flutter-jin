a3ea160b325c8921abc32abcfec656aa578c3643
greencoffee1635 <166354@naver.com>
2022-04-20 19:36:11 +0900

    박스 디자인 alignment 추가

--- contact/lib/main.dart
+++ contact/lib/main.dart
@@ -14,14 +14,12 @@ class MyApp extends StatelessWidget {
       // Flutter에서 앱 디자인 넣는 법 : 위젯 짜깁기
       home: Scaffold( // App을 상, 중, 하로 나눠준다.
         appBar: AppBar(title: Text('앱임')),
-        body: Container(
-          width: 150, height: 50,
-          margin: EdgeInsets.all(20), // 바깥 여백 주고 싶으면, margin Parameter를 이용한다.
-          // padding: EdgeInsets.fromLTRB(left, top, right, bottom),
-          // padding: EdgeInsets.fromLTRB(0, 30, 0, 0),
-          decoration: BoxDecoration( // 테두리 주고 싶으면 decoration Parameter를 준다.
-            border: Border.all(color: Colors.black)
-          )
+        body: Align(
+          // alignment: Alignment.bottomCenter, // 하단
+          alignment: Alignment.topCenter,
+          child: Container(
+            width: double.infinity, height: 50, color: Colors.blue, // 박스 width를 가로로 꽉차게
+          ),
         )
       )
     );
