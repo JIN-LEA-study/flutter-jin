e94fd9baef51b024467d946f3aec55ea4dead808
greencoffee1635 <166354@naver.com>
2022-04-22 21:57:38 +0900

    ListView 연습

--- contact/lib/main.dart
+++ contact/lib/main.dart
@@ -14,23 +14,16 @@ class MyApp extends StatelessWidget {
       // Flutter에서 앱 디자인 넣는 법 : 위젯 짜깁기
       home: Scaffold( // App을 상, 중, 하로 나눠준다.
         appBar: AppBar(),
-        body: ShopItem(),
+        body: ListView( // ListView() 안에 넣으면 스크롤바 생성, 스크롤 위치 감시기능, 메모리 절약기능.
+          children: [
+            Text('안녕'),
+            Text('안녕'),
+            Text('안녕'),
+            Text('안녕'),
+            Text('안녕'),
+          ],
+        )
       )
-    );
-  }
-}
-// 커스텀 위젯 만들기
-// stless + tab
-// class를 복사해서 새로운 class 만들고 싶으면 extends 키워드 쓰면 된다.
-// 복사하기 좋은 완성된 위젯 class가 StatelessWidget 이라는 것이고
-// 그걸 복사하면 커스텀 위젯 쉽게 만들 수 있다.
-// class 작명 extends StatelessWidget {}
-class ShopItem extends StatelessWidget {
-  const ShopItem({Key? key}) : super(key: key);
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      child: Text('안녕'),
     );
   }
 }
