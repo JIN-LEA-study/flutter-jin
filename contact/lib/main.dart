b2b3ba6eaa3452904ee21e48c87f70f1476bd21c
greencoffee1635 <166354@naver.com>
2022-05-06 13:52:51 +0900

    Dialog 생성 및 context 정의

--- contact/lib/main.dart
+++ contact/lib/main.dart
@@ -1,29 +1,13 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp()); // 앱 구동 함수
+  runApp(
+      MaterialApp(
+        home: MyApp())
+  );
 }
 
-// stful + Tab키
-// state는 StatefulWidget 안에서만 생성 가능
-// state 생성 방법 1.
-// class Test extends StatefulWidget {
-//   const Test({Key? key}) : super(key: key);
-//
-//   @override
-//   State<Test> createState() => _TestState();
-// }
-//
-// class _TestState extends State<Test> {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Container();
-//   }
-// }
-
-
-// state 생성 방법 2.
-// StatelessWidget을 StatefulWidget으로 변환 (전구 -> Convert to StatefulWidget)
+// MyApp이라는 customWidget 바깥으로 MaterialApp()을 보낸다
 class MyApp extends StatefulWidget {
   MyApp({Key? key}) : super(key: key);
   @override
@@ -36,10 +20,21 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
 
-    return MaterialApp(
-      home: Scaffold( // App을 상, 중, 하로 나눠준다.
+    return Scaffold( // 보통 첫 페이지는 Scaffold()부터 시작
+        floatingActionButton: FloatingActionButton(
+          onPressed: (){ // 두 파라미터(context, builder)를 채워줘야 한다.
+            // print(context.findAncestorWidgetOfExactType<MaterialApp>()); //context에 MaterialApp이 있는지 확인
+            showDialog(context: context, builder: (context){
+            // context는 customWidget을 만들때마다 강제로 하나씩 생성된다.
+            // context 구글 정의: The build location of the current widget.
+            // 부모 위젯의 정보를 담고 있는 변수 (형제없는 족보)
+              return Dialog(child: Text('안녕'));
+            });
+          },
+        ),
         appBar: AppBar(),
-        body: ListView.builder(
+        // body: Dialog(child: Text('Dialog'),) // Dialog() 사용
+        body:ListView.builder(
           itemCount: 3,
           itemBuilder:(c, i){
             return ListTile(
@@ -47,8 +42,21 @@ class _MyAppState extends State<MyApp> {
               title: Text(name[i]),
             );
           }
-        )
       )
     );
+  }
+}
+
+class Test extends StatefulWidget {
+  const Test({Key? key}) : super(key: key);
+
+  @override
+  State<Test> createState() => _TestState();
+}
+
+class _TestState extends State<Test> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
   }
 }
