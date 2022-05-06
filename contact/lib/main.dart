8b9e4d347bc2674b6ebcad21d073145b2043f199
greencoffee1635 <166354@naver.com>
2022-05-06 14:19:46 +0900

    Builder() 추가

--- contact/lib/main.dart
+++ contact/lib/main.dart
@@ -17,20 +17,30 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   var a = 1;
   var name = ['김영숙', '홍길동', '피자집'];
+  var like = [0, 0, 0];
+
   @override
-  Widget build(BuildContext context) {
+  build(context) {
+  // build(jokbo) {
 
     return Scaffold( // 보통 첫 페이지는 Scaffold()부터 시작
-        floatingActionButton: FloatingActionButton(
-          onPressed: (){ // 두 파라미터(context, builder)를 채워줘야 한다.
-            // print(context.findAncestorWidgetOfExactType<MaterialApp>()); //context에 MaterialApp이 있는지 확인
-            showDialog(context: context, builder: (context){
-            // context는 customWidget을 만들때마다 강제로 하나씩 생성된다.
-            // context 구글 정의: The build location of the current widget.
-            // 부모 위젯의 정보를 담고 있는 변수 (형제없는 족보)
-              return Dialog(child: Text('안녕'));
-            });
-          },
+        floatingActionButton: Builder( //Builder 족보 생성기
+          builder: (jokbo1) { // Scaffold, MaterialApp 이런게 들어있다.
+            return FloatingActionButton(
+              onPressed: (){ // 두 파라미터(context, builder)를 채워줘야 한다.
+                // print(context.findAncestorWidgetOfExactType<MaterialApp>()); //context에 MaterialApp이 있는지 확인
+                showDialog(context: context, builder: (context){
+                // showDialog(context: jokbo, builder: (context){
+                // context는 customWidget을 만들때마다 강제로 하나씩 생성된다.
+                // context 구글 정의: The build location of the current widget.
+                // 부모 위젯의 정보를 담고 있는 변수 (형제없는 족보)
+                // showDialog(context) Scaffold.of(context)
+                // Navigator.pop(context) Theme.of(context) - context입력해야 동작하는 함수들
+                  return Dialog(child: Text('안녕'));
+                });
+              },
+            );
+          }
         ),
         appBar: AppBar(),
         // body: Dialog(child: Text('Dialog'),) // Dialog() 사용
@@ -44,19 +54,5 @@ class _MyAppState extends State<MyApp> {
           }
       )
     );
-  }
-}
-
-class Test extends StatefulWidget {
-  const Test({Key? key}) : super(key: key);
-
-  @override
-  State<Test> createState() => _TestState();
-}
-
-class _TestState extends State<Test> {
-  @override
-  Widget build(BuildContext context) {
-    return Container();
   }
 }
