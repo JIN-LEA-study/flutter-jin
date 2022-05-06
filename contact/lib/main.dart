f7542cf3f67e7cbf6636fe28203d5cc4d7d975f5
greencoffee1635 <166354@naver.com>
2022-05-06 14:54:19 +0900

    부모위젯에서 자식위젯으로 state 전송

--- contact/lib/main.dart
+++ contact/lib/main.dart
@@ -7,7 +7,6 @@ void main() {
   );
 }
 
-// MyApp이라는 customWidget 바깥으로 MaterialApp()을 보낸다
 class MyApp extends StatefulWidget {
   MyApp({Key? key}) : super(key: key);
   @override
@@ -15,43 +14,57 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  var a = 1;
+  var a = 3;
   var name = ['김영숙', '홍길동', '피자집'];
   var like = [0, 0, 0];
 
   @override
-  build(context) {
-  // build(jokbo) {
-
-    return Scaffold( // 보통 첫 페이지는 Scaffold()부터 시작
-        floatingActionButton: Builder( //Builder 족보 생성기
-          builder: (jokbo1) { // Scaffold, MaterialApp 이런게 들어있다.
-            return FloatingActionButton(
-              onPressed: (){ // 두 파라미터(context, builder)를 채워줘야 한다.
-                // print(context.findAncestorWidgetOfExactType<MaterialApp>()); //context에 MaterialApp이 있는지 확인
-                showDialog(context: context, builder: (context){
-                // showDialog(context: jokbo, builder: (context){
-                // context는 customWidget을 만들때마다 강제로 하나씩 생성된다.
-                // context 구글 정의: The build location of the current widget.
-                // 부모 위젯의 정보를 담고 있는 변수 (형제없는 족보)
-                // showDialog(context) Scaffold.of(context)
-                // Navigator.pop(context) Theme.of(context) - context입력해야 동작하는 함수들
-                  return Dialog(child: Text('안녕'));
-                });
-              },
-            );
-          }
-        ),
+  Widget build(BuildContext context) {
+    return Scaffold(
         appBar: AppBar(),
-        // body: Dialog(child: Text('Dialog'),) // Dialog() 사용
-        body:ListView.builder(
+        body: ListView.builder(
           itemCount: 3,
           itemBuilder:(c, i){
             return ListTile(
               leading: Image.asset('profile.png'),
               title: Text(name[i]),
             );
-          }
+          },
+        ),
+        // 부모 -> 자식 state 전송 방법 1. 보내고 2. 등록하고 3. 사용
+        floatingActionButton: FloatingActionButton(
+              onPressed: (){
+                showDialog(context: context, builder: (context){
+                  return DialogUI(state : a, state2: 'melon');
+                  // 1. 자식위젯 (작명: 보낼 state)
+                });
+              },
+          ),
+        );
+  }
+}
+
+class DialogUI extends StatelessWidget {
+  DialogUI({Key? key, this.state, this.state2 }) : super(key: key);
+  final state;
+  final state2;
+  // state로 작명한 것을 line48, line49 두군데에 등록
+  // var state; -> 다음과 같이 작성시 다른 const를 삭제해야한다 귀찮으면 final을 쓰는데 다만 final은 최종이라 수정이 안된다.
+  // 부모가 보낸 state는 read-only 가 좋다. 그래서 final을 쓰는 것도 좋다.
+  // 2. 등록은 2곳에
+  @override
+  Widget build(BuildContext context) {
+    return Dialog(
+      child: SizedBox(
+        width: 300,
+        height: 300,
+        child: Column(
+          children: [
+            TextField(),
+            TextButton(onPressed: (){}, child: Text(state2.toString())),
+            TextButton(onPressed: (){ Navigator.pop(context); }, child: Text('취소')) // 실행시 닫힘
+          ],
+        )
       )
     );
   }
