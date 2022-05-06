16a3ecc3d027b3c5f7cdba5c429e543d1de7fd50
greencoffee1635 <166354@naver.com>
2022-05-06 18:03:28 +0900

    유저에게 앱 권한 요청 작업

--- contact/lib/main.dart
+++ contact/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 void main() {
   runApp(
@@ -14,6 +15,27 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+
+  getPermission() async {
+    var status = await Permission.contacts.status; // 연락처 권한 줬는지 여부
+    // Dart 특징 : 오래 걸리는 줄을 제껴두고 다음 줄 실행하려고 함
+    // await 붙으면 다음 줄 실행안하고 기다려준다. (async붙어 있어야 안에서 await 사용 가능, 임의 X, Future를 리턴하는 함수에만 붙일 수 있다.)
+    if (status.isGranted) {
+      print('허락됨'); // 허락하면 실행
+    } else if (status.isDenied) {
+      print('거절됨'); // 허락안하면/미선택이면 실행
+      Permission.contacts.request(); // 허락해달라고 팝업띄우는 코드
+      openAppSettings(); // 앱설정화면 켜줌 - 거절당하면 유저가 앱설정 들어가서 직접 권한을 켜야함
+    }
+  } // 패키지 사용법일뿐
+
+  // @override // 처음 로드될 때 이 코드를 실행해달라
+  // void initState() { // initState 안에 적은 코드는 위젯 로드될 때 한번 실행된다.
+  //   super.initState();
+  //   getPermission(); // 앱 정책상 deny 2번이상 하면 다시 팝업 안뜨기 때문에 allow 잘 누르기
+  // }
+  // // 처음 로드되자마자 권한요구하는 것보다
+  // // 연락처가 본격적으로 필요할 때 실행해주는 것이 좋은 것 같다.
 
   var total = 3;
   var name = ['김영숙', '홍길동', '피자집']; // 부모위젯에 있는 state 자식위젯에서 수정하려고 한다.
@@ -37,13 +59,15 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar( title : Text(total.toString()),),
+        appBar: AppBar( title : Text(total.toString()), actions: [
+          IconButton(onPressed: (){ getPermission(); }, icon: Icon(Icons.contacts))
+        ]),
         body: ListView.builder(
           // itemCount: 3,
           itemCount: name.length, // itemCount: 3이 아닌 동적으로 변경, name 자료길이 만큼 반복되도록 수정.
           itemBuilder:(c, i){
             return ListTile(
-              leading: Image.asset('profile.png'),
+              leading: Image.asset('assets/profile.png', width: 100,),
               title: Text(name[i]),
             );
           },
