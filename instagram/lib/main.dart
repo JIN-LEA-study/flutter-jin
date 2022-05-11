6bd6424d2268c616975c18cda6d188e2b183ec9e
greencoffee1635 <166354@naver.com>
2022-05-12 02:16:55 +0900

    style.dart 파일 분리

--- instagram/lib/main.dart
+++ instagram/lib/main.dart
@@ -1,44 +1,35 @@
 import 'package:flutter/material.dart';
+import 'style.dart';
+// import 'style.dart' as style; 같이 작명가능 style.theme 이같은 방식으로 사용
+// 다른 파일에 있는 변수, 함수 쓰고 싶으면 import'파일경로'부터
+// import './style.dart'; - OK - './'는 현재 경로를 뜻한다.
 
 void main() {
   runApp(
       MaterialApp( // MaterialApp()을 밖으로 빼야 나중에 편하다.
-          theme: ThemeData(
-            // ThemeData 사용시 특징 1. 위젯은 나랑 가까운 스타일을 가장 먼저 적용
-            // ThemeData 사용시 특징 2. 복잡한 위젯은 복잡한위젯Theme()안에서 스타일줘야할 수도 (여기서 아이콘 스타일 줘봤자 AppBar안은 불가능)
-            iconTheme: IconThemeData( color: Colors.blue ),
-            appBarTheme: AppBarTheme(
-                color: Colors.grey,
-                actionsIconTheme: IconThemeData( color: Colors.blue )
-            ),
-            textTheme: TextTheme(
-              bodyText2: TextStyle( color:Colors.red )
-            ),
-          ), // 스타일 모아놓는 ThemeData() <style></style> 스타일태그나 css파일 같은 부류
+          theme: theme,
           home : MyApp()
       )
   );
 }
 
-// Text() 위젯 스타일 결정하고 싶다면
-// Text()는 bodyText2 가져다 쓰고
-// ListTile()은 subtitle1 가져다 쓰고
-// TextButton()은 button 가져다 쓰고
-// AppBar()은 headline6 가져다 쓰고
-// 전체스타일을 주고싶다 -> 위와 같이 사용
-// but, 이렇게 하면 복잡하다
-// 그냥 변수로 축약해서 쓰는게 낫다
-
-var a = TextStyle();
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar( actions: [Icon(Icons.star)] ),
-      // body: Text('안녕', style: TextStyle(),), // 직접 입력하는 방식
-      body: Text('안녕', style: a ), // 변수로 축약해서 사용하는 방식
+      appBar: AppBar(
+          title: Text('Instagram'),
+          actions: [
+            IconButton(
+              icon: Icon(Icons.add_box_outlined),
+              onPressed: (){},
+              iconSize: 30,
+            )
+          ]
+      ),
+      body: TextButton(onPressed: (){}, child: Text('안녕'),),
     );
   }
 }
---
+++ instagram/lib/style.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+// 기본 위젯들 쓰려면 필요하다
+
+// 이 변수는 다른 파일로 안들어갔으면 좋겠다 - 변수를 다른 파일에 쓰기 싫으면 _변수명_함수명_클래스명
+// 왼쪽 언더바를 사용하면 이 변수는 다른 파일에서 절대 쓸 수 없는 변수가 된다.
+
+var theme = ThemeData( // ThemeData()안의 버튼 스타일은
+  textButtonTheme: TextButtonThemeData(
+    style: TextButton.styleFrom(
+      backgroundColor: Colors.grey, // 모든 TextButton()이 그레이가 된다.
+    )
+  ),
+  // elevatedButtonTheme: 그림자 버튼,
+  appBarTheme: AppBarTheme(
+    color: Colors.white,
+    elevation: 1, // 그림자 크기
+    titleTextStyle: TextStyle(color:Colors.black, fontSize: 25),
+    actionsIconTheme: IconThemeData(color: Colors.black),
+  ),
+  textTheme: TextTheme(
+      bodyText2: TextStyle( color:Colors.red )
+  ),
+);
