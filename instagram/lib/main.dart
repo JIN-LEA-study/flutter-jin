b3f637a9957ce15f3ee39f9d9521545a5a92c76e
greencoffee1635 <166354@naver.com>
2022-05-12 19:17:17 +0900

    스크롤 위치 파악 후 더보기 요청

--- instagram/lib/main.dart
+++ instagram/lib/main.dart
@@ -1,16 +1,13 @@
 import 'package:flutter/material.dart';
-import 'style.dart';
+import 'style.dart' as style;
 import 'package:http/http.dart' as http;
 import 'dart:convert';
-
-// import 'style.dart' as style; 같이 작명가능 style.theme 이같은 방식으로 사용
-// 다른 파일에 있는 변수, 함수 쓰고 싶으면 import'파일경로'부터
-// import './style.dart'; - OK - './'는 현재 경로를 뜻한다.
+import 'package:flutter/rendering.dart';  // 스크롤 관련 유용한 함수  // 스크롤 다룰때 import 해두고 쓰는게 좋다.
 
 void main() {
   runApp(
       MaterialApp(
-          theme: theme,
+          theme: style.theme,
           home : MyApp()
       )
   );
@@ -24,27 +21,27 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  var tab = 1;
-  var data = [];  // 데이터 들어오는데 몇초 걸린다.
+  var tab = 0;
+  var data = [];
+
+  addData(a) {
+    setState(() {
+      data.add(a);  // .add()하면 리스트 안에 자료 추가 가능
+    });
+  }
 
   getData() async {
-    var result = await http.get(Uri.parse('https://codingapple1.github.io/app/data.json')); // 오래 걸리는 함수(전문용어로 Future, Future를 뱉는 함수)
-    // 여러가지 예외처리 필요
-    // - 서버가 다운되었거나
-    // - 요청 경로가 이상하거나
-    // if (result.statusCode == 200) {  // 성공시 보통 200이 남는다, 실패시 4XX, 5XX
-    //
-    // } else{  // 실패시
-    //
-    // }
+    var result = await http.get(Uri.parse('https://codingapple1.github.io/app/data.json'));  // 오래 걸리는 함수 (전문용어로 Future, Future를 뱉는 함수)
+    if (result.statusCode == 200) {  // 성공시 보통 200이 남는다, 실패시 4XX, 5XX
 
-    // Dio 패키지 설치하면 GET요청이 좀 더 짧아진다.
+    } else {  // 실패시
 
-    var result2 = jsonDecode(result.body); // JSON->[],{} 변환해서 쓴다. (jsonDecode)
+    }
+    var result2 = jsonDecode(result.body);  // JSON->[],{} 변환해서 쓴다. (jsonDecode)
+
     setState(() {
       data = result2;
     });
-    print(result2[0]['likes']);
   }
 
   // 로드 되면 바로 GET 요청
@@ -67,28 +64,15 @@ class _MyAppState extends State<MyApp> {
             )
           ]
       ),
-      // future: 에 입력한 Future가 완료되면, builder: 안의 위젯을 보여준다.
-      // FutureBuilder위젯: Future 변수가 실제 데이터로 변할 때 내부 코드 1회 실행해주는 위젯
-      // - 데이터가 자주 추가되면 쓰기 불편하다.
-      // - 데이터가 나중에 추가 안되는 경우 유용
-      body: [FutureBuilder(
-          future: http.get('어쩌구'),
-          builder: (context, snapshot){
-            if (snapshot.hasData) {
-              return Text('post에 데이터 있으면 보여줄 위젯')
-            }
-            return Text('post에 데이터 없으면 보여줄 위젯')
-          },
-      ), Text('샵페이지')][tab],
+      body: [Home(data: data, addData: addData ), Text('샵페이지')][tab],
       bottomNavigationBar: BottomNavigationBar(
         showSelectedLabels: false,
         showUnselectedLabels: false,
-        onTap:(i){
-          // print(i);
+        onTap:(i) {
           setState(() {
             tab = i;
           });
-        }, // 기본적으로 파라미터 작성하게 되어있다. i는 지금 누른 버튼 번호
+        },
         items:[
           BottomNavigationBarItem(icon: Icon(Icons.home_outlined), label:'홈'),
           BottomNavigationBarItem(icon: Icon(Icons.shopping_bag_outlined), label:'샵'),
@@ -98,36 +82,59 @@ class _MyAppState extends State<MyApp> {
   }
 }
 
-class Home extends StatelessWidget {
-  const Home({Key? key, this.data}) : super(key: key);
-  final data; // final, 부모가 보낸건 보통 수정하지 않는다.
+class Home extends StatefulWidget {  // 스크롤 바 높이 측정하려면, 우선 ListView담은 곳이 StatefulWidget이어야 한다. 전구이용해서 변경한다.
+  const Home({Key? key, this.data, this.addData}) : super(key: key);  // 부모가 보낸 state 등록은 첫번째 class에
+  final data;
+  final addData;
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {  // 사용은 두번째 class에
+  var scroll = ScrollController();  // 위치 측정은 스크롤 움직일 때마다 해야한다.(바닥인지 계속 체크해야)
+
+  getMore() async {
+    var result = await http.get(Uri.parse('https://codingapple1.github.io/app/more1.json'));
+    var result2 = jsonDecode(result.body);
+    widget.addData(result2);
+  }
+
+  @override
+  void initState() {
+    super.initState();  // 스크롤바 높이 측정하려면 - 리스너 부착하기
+    scroll.addListener(() {  // scroll 변수가 변할 때마다 addlistener로 특정 코드 실행  // 필요없어지면 제거하는 것도 성능상 좋다.
+      if (scroll.position.pixels == scroll.position.maxScrollExtent){
+        // print('같음');
+        getMore();
+      }
+      // print(scroll.position.maxScrollExtent);  // 스크롤바 최대 내릴 수 있는 높이
+      // print(scroll.position.userScrollDirection);  // 스크롤 되는 방향 (ScrollDirection.forward, ScrollDirection.reverse)
+   });
+  }
 
   @override
   Widget build(BuildContext context) {
-    // print(data); // print()는 함수 안에서 쓰면 된다.
-
-    if (data.isNotEmpty){  // data에 뭐 들어있으면 보여달라
-      return ListView.builder(itemCount: 3, itemBuilder: (c, i){ // i는 1씩 증가하는 정수
-        return Container(
-          constraints: BoxConstraints(maxWidth: 600), // constraints 최대 폭을 줄 수 있다
-          padding: EdgeInsets.all(20),
-          width: double.infinity,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Image.network(data[i]['image']),
-              // Image.network('웹이미지주소') 웹에 올려진 이미지
-              Text('좋아요 100'),
-              Text('글쓴이'),
-              // Text(data[0]['content']),
-              Text(data[i]['content']),
-              // 데이터 들어오기도 전에 data[i]['content']하면 에러난다.
-            ],
-          ),
-        );
-      });
+    // print(widget.addData);
+    if (widget.data.isNotEmpty) {  // 첫 class안에 있던 변수 사용은 widget.변수명
+      return ListView.builder(itemCount: widget.data.length, controller: scroll, itemBuilder: (c, i) {  // 유저가 얼마나 스크롤 했는지 정보가 scroll 변수에 저장된다.
+            return Container(
+              constraints: BoxConstraints(maxWidth: 600),
+              padding: EdgeInsets.all(20),
+              width: double.infinity,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Image.network(widget.data[i]['image']),
+                  Text('좋아요 ${widget.data[i]['likes']}'),
+                  Text(widget.data[i]['date']),
+                  Text(widget.data[i]['content']),
+                ],
+              ),
+            );
+          });
     } else {
-      return Text('로딩중임'); // CircularProgressIndicator()
-   }
+      return CircularProgressIndicator();
+    }
   }
 }
