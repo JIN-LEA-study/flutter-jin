3e07e8bbdcee10ee13fbe072c71bbe45b8a78085
greencoffee1635 <166354@naver.com>
2022-05-12 14:40:30 +0900

    탭으로 페이지 나누기 - onTap이용하여 동적인 UI 생성

--- instagram/lib/main.dart
+++ instagram/lib/main.dart
@@ -6,15 +6,30 @@ import 'style.dart';
 
 void main() {
   runApp(
-      MaterialApp( // MaterialApp()을 밖으로 빼야 나중에 편하다.
+      MaterialApp(
           theme: theme,
           home : MyApp()
       )
   );
 }
 
-class MyApp extends StatelessWidget {
+// 동적인 UI 만드는 법
+// 1. state에 tab의 현재 상태 저장
+// 2. state에 따라 tab이 어떻게 보일지 작성
+// 3. 유저가 쉽게 state 조작할 수 있게 -> 누르면 state 바뀌는 버튼 만들기
+
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+} // 여기까지 5줄 무시
+
+class _MyAppState extends State<MyApp> {
+  var tab = 1;
+
+  // state가 0이면, Text('홈')
+  // state가 1이면, Text('샵')
 
   @override
   Widget build(BuildContext context) {
@@ -29,11 +44,17 @@ class MyApp extends StatelessWidget {
             )
           ]
       ),
-      // body: Text('안녕', style: Theme.of(context).textTheme.bodyText1,) // ThemeData() 찾아서 거기 있던 textTheme>bodyText2 가져오기  // 원하는 ThemeData안의 내용 불러오기
-      body: Text('안녕'),
+      // body: [Text('홈페이지'),Text('샵페이지')][1],
+      body: [Text('홈페이지'), Text('샵페이지')][tab],
       bottomNavigationBar: BottomNavigationBar(
         showSelectedLabels: false,
         showUnselectedLabels: false,
+        onTap:(i){
+          // print(i);
+          setState(() {
+            tab = i;
+          });
+        }, // 기본적으로 파라미터 작성하게 되어있다. i는 지금 누른 버튼 번호
         items:[
           BottomNavigationBarItem(icon: Icon(Icons.home_outlined), label:'홈'),
           BottomNavigationBarItem(icon: Icon(Icons.shopping_bag_outlined), label:'샵'),
