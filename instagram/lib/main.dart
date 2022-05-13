8791924cbfc042e3fad39ccf61c39e1a117ef7de
greencoffee1635 <166354@naver.com>
2022-05-13 15:37:39 +0900

    프로필 페이지 - Grid 사용하여 유저 사진 보여주기

--- instagram/lib/main.dart
+++ instagram/lib/main.dart
@@ -327,16 +327,15 @@ class Profile extends StatelessWidget {
       appBar: AppBar(title: Text(context.watch<Store2>().name),),
       body: CustomScrollView(  // 각각이 아닌 전체 스크롤바 생성해주는 CustomScrollView() children이 아닌 slivers로 작성해야 한다.
         slivers: [  // slivers 안에 있는 것들을 다 합쳐서 스크롤바를 만들어 줌
-          SliverToBoxAdapter(
-            child: ProfileHeader(),
-          ),
+          SliverToBoxAdapter(child: ProfileHeader()),
           SliverGrid(delegate: SliverChildBuilderDelegate(
-                (c, i) => Container(color: Colors.grey),
-                childCount: 3,  // 격자 몇개 만들 것
-          ), gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),),
+                (c, i) => Image.network(context.watch<Store1>().profileImage[i]),
+            childCount: context.watch<Store1>().profileImage.length,
+          ),  // 격자 몇개 만들 것
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount( crossAxisCount: 2 )),
           // SliverList(delegate: delegate),
           // SliverAppBar(),
-
+        ],
           // slivers 안에는 평소에 쓰던 위젯을 못 넣는다.
           // 격자 넣고 싶으면 SliverGrid()
           // ListView넣고 싶으면 SliverList()
@@ -350,9 +349,7 @@ class Profile extends StatelessWidget {
           //   },
           //   itemCount: 3,
           // ),
-        ],
       )
-
     );
   }
 }
